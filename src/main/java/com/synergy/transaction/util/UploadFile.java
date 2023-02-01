e408d3d1db0a345e00fbd04e9e0fab7d54ebb816
Akin01 <Akinpasha82@gmail.com>
2023-02-02 00:00:22 +0800

    creating response utilities and cloudinary configuration

---
+++ src/main/java/com/synergy/transaction/util/UploadFile.java
@@ -0,0 +1,25 @@
+package com.synergy.transaction.util;
+
+import com.cloudinary.Cloudinary;
+import com.synergy.transaction.config.CloudFolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UploadFile {
+    @Autowired
+    Cloudinary cloudinary;
+
+    public String UploadSingleFile(MultipartFile file, String folderName) throws IOException {
+        Map<String, Object> options = new HashMap<>();
+        options.put("folder", folderName);
+        Map uploadedFile = cloudinary.uploader().upload((Object) file.getBytes(), options);
+        String publicId = (String) uploadedFile.get("public_id");
+        return cloudinary.url().secure(true).generate(publicId);
+    }
+}
